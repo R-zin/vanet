7a740dd8a2ea4d88a4c921d17a6cfcd28924c9bb
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-18 23:36:48 +0530

    Added .cc file for gpsr routing

--- src/gpsr/model/gpsr-routing-protocol.h
+++ src/gpsr/model/gpsr-routing-protocol.h
@@ -21,39 +21,22 @@
 #include <map>
 
 namespace ns3 {
-
 class GpsrRoutingProtocol : public Ipv4RoutingProtocol
-
 {
-
 public:
-
     static TypeId GetTypeId (void);
-
     GpsrRoutingProtocol ();
-
     virtual ~GpsrRoutingProtocol ();
-
     // Inherited from Ipv4RoutingProtocol
-
     virtual Ptr<Ipv4Route> RouteOutput (Ptr<Packet> p, const Ipv4Header &header, Ptr<NetDevice> oif, Socket::SocketErrno &sockerr);
-
     virtual bool RouteInput (Ptr<const Packet> p, const Ipv4Header &header, Ptr<const NetDevice> idev,
-
-                             UnicastForwardCallback ucb, MulticastForwardCallback mcb, LocalDeliverCallback lcb, ErrorCallback ecb);
-
+    UnicastForwardCallback ucb, MulticastForwardCallback mcb, LocalDeliverCallback lcb, ErrorCallback ecb);
     virtual void NotifyInterfaceUp (uint32_t interface);
-
     virtual void NotifyInterfaceDown (uint32_t interface);
-
     virtual void NotifyAddAddress (uint32_t interface, Ipv4InterfaceAddress address);
-
     virtual void NotifyRemoveAddress (uint32_t interface, Ipv4InterfaceAddress address);
-
     virtual void SetIpv4 (Ptr<Ipv4> ipv4);
-
     virtual void PrintRoutingTable (Ptr<OutputStreamWrapper> stream) const;
-
 private:
     Ptr<Ipv4> m_ipv4;
     std::map<Ipv4Address, Ipv4RoutingTableEntry> m_routingTable;
